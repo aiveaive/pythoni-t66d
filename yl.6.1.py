f9c205e69ba268a5a4a2a02b2b37fd921aaf6ca5
aiveaive <aive3@hot.ee>
2019-05-15 15:20:09 +0300

    parandatud

--- yl.6.1.py
+++ yl.6.1.py
@@ -1,6 +1,6 @@
+from math import pi
+
 r = float(input("Sisesta ringi raadius: "))
-c = 2*3.14*r
-a = 3.14*r**2
-print ("Ringi ümbermööt on: " + str(c) + ". Ringi pindala on: " + str (a))
-
-
+c = 2*pi*r
+a = pi*r**2
+print("Ringi ümbermööt on: " + str(round(c, 2)) + ". Ringi pindala on: " + str (round(a, 2)))
